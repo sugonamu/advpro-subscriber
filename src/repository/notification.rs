1848dc0af0d27c0aa2c6e99c9f24adc908600f9f
sugonamu <je0jwff@gmail.com>
2025-03-26 16:19:00 +0700

    Implement add function in Subscriber repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -1,15 +1,20 @@
 use std::sync::RwLock;
-  
- use lazy_static::lazy_static;
-  
- use crate::model::notification::Notification;
-  
- // Singleton of Database
- lazy_static! {
-     static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
- }
-  
- pub struct NotificationRepository;
  
- impl NotificationRepository {
- }
+use lazy_static::lazy_static;
+ 
+use crate::model::notification::Notification;
+ 
+// Singleton of Database
+lazy_static! {
+    static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
+}
+ 
+pub struct NotificationRepository;
+
+impl NotificationRepository {
+    pub fn add(notification: Notification) -> Notification {
+        NOTIFICATIONS.write().unwrap()
+            .push(notification.clone());
+        return notification;
+    }
+}
